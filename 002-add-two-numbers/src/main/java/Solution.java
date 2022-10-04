8b21a4ceab213925b2e7c74a63d71f6d557350c9
Damien Frémont <damien.fremont@gmail.com>
2022-10-04 23:11:36 +0200

    feat(002): example33

--- 002-add-two-numbers/src/main/java/Solution.java
+++ 002-add-two-numbers/src/main/java/Solution.java
@@ -10,28 +10,30 @@
  * }
  */
 class Solution {
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        var num1 = toInt(l1);
-        var num2 = toInt(l2);
+        var num1 = toNumber(l1);
+        var num2 = toNumber(l2);
         var res = num1 + num2;
         System.out.println("Explanation: " + num1 + " + " + num2 + " = " + res);
+        System.out.println("Output: "+res);
         return toListNode(res);
     }
 
-    private int toInt(ListNode node) {
-        return toIntLoop(node, 0, 1);
+    private long toNumber(ListNode node) {
+        return toNumberLoop(node, 0, 1);
     }
 
-    private int toIntLoop(ListNode node, int val, int order) {
+    private long toNumberLoop(ListNode node, long val, long order) {
         var res = val + (node.val * order);
         if (node.next != null) {
-            return toIntLoop(node.next, res, order * 10);
+            return toNumberLoop(node.next, res, order * 10);
         }
         return res;
     }
 
-    private ListNode toListNode(int arg) {
-        var chars = Integer.toString(arg);
+    private ListNode toListNode(long arg) {
+        var chars = Long.toString(arg);
         ListNode last = null;
         for (int i = 0; i < chars.length(); i++) {
             var c = chars.charAt(i);
--- 002-add-two-numbers/src/test/java/SolutionTest.java
+++ 002-add-two-numbers/src/test/java/SolutionTest.java
@@ -6,18 +6,80 @@ class SolutionTest {
 
     @Test
     void example1() {
-        var cut = new Solution();
+        var arg1 = new int[]{2, 4, 3};
+        var arg2 = new int[]{5, 6, 4};
+        var exp = new int[]{7, 0, 8};
 
-        var arg1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-        var arg2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-        var expc = new ListNode(7, new ListNode(0, new ListNode(8)));
+        var res = when(arg1, arg2);
 
-        var res = cut.addTwoNumbers(arg1, arg2);
-
-        assertNotNull(res);
-        assertEquals(expc.val, res.val);
-        assertEquals(expc.next.val, res.next.val);
-        assertEquals(expc.next.next.val, res.next.next.val);
+        then(exp, res);
     }
 
+    @Test
+    void example2() {
+        var arg1 = new int[]{0};
+        var arg2 = new int[]{0};
+        var exp = new int[]{0};
+
+        var res = when(arg1, arg2);
+
+        then(exp, res);
+    }
+
+    @Test
+    void example3() {
+        var arg1 = new int[]{9, 9, 9, 9, 9, 9, 9};
+        var arg2 = new int[]{9, 9, 9, 9};
+        var exp = new int[]{8, 9, 9, 9, 0, 0, 0, 1};
+
+        var res = when(arg1, arg2);
+
+        then(exp, res);
+    }
+
+    @Test
+    void example33() {
+        var arg1 = new int[]{9};
+        var arg2 = new int[]{1, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+        var exp = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+
+        var res = when(arg1, arg2);
+
+        then(exp, res);
+    }
+
+    private void then(int[] exp, ListNode res) {
+        System.out.println("Expected: " + toString(exp));
+        assertTrue(res != null);
+        var last = res;
+        for (var xxx : exp) {
+            assertEquals(xxx, last.val);
+            last = last.next;
+        }
+    }
+
+    private ListNode when(int[] arg1, int[] arg2) {
+        var cut = new Solution();
+        var res = cut.addTwoNumbers(toListNode(arg1), toListNode(arg2));
+        return res;
+    }
+
+    private String toString(int[] exp) {
+        var res = "";
+        for (var i : exp) {
+            res = res + i;
+        }
+        return res;
+    }
+
+    private ListNode toListNode(int[] args) {
+        System.out.println("Input:" + args);
+        ListNode last = null;
+        for (int i = args.length - 1; i >= 0; i--) {
+            var curr = new ListNode(args[i], last);
+            System.out.println(curr.val);
+            last = curr;
+        }
+        return last;
+    }
 }
